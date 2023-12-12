83f4896a30defc194592a3924bf24b2ed071c092
j. redhead <jeremyredhead@users.noreply.github.com>
2023-12-12 00:53:58 -0600

    make work on subdimoan
    also why the frick has none of GM, TM, VM auto-updated the script yet it's been foooreeeeveeerr AIIIEEEE

--- script.user.js
+++ script.user.js
@@ -2,7 +2,7 @@
 // @name          just testing
 // @description   this is a test to make sure that stuff works y'know?
 // @version       0.0.2
-// @match         *://example.com/*
+// @match         *://*.example.com/*
 // ==/UserScript==
 
 // this is broken cuz it does it repatedly if you uhh back& forwrad bnut uhh this is actually Good:tm: You'll see.. YOU"LL ALL SEE!!!1
