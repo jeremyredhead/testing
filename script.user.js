e875b8ba51753739ff241a37de8afa1a35e8dbbc
j. redhead <jeremyredhead@users.noreply.github.com>
2023-12-11 23:34:03 -0600

    add a user's script
    a parched mint wrotten [sic] in the slang gauge of ""User""

---
+++ script.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name          just testing
+// @description   this is test make sure that uhhh stuff worky yea yknow/??
+// @version       0.0.1
+// @match         *://example.com/*
+// ==/UserScript==
+
+// this is broken cuz it does it repatedly if you uhh back& forwrad bnut uhh this is actually Good:tm: You'll see.. YOU"LL ALL SEE!!!1
+document.querySelector('h1').textContent += ': ' + location.pathname
